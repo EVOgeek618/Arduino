0dfbf9f55aec4c28e511c2e93525e3d92c31d28b
EVOgeek618 <37446747+EVOgeek618@users.noreply.github.com>
2019-02-07 20:05:30 +0300

    Update Rmetr.ino

--- Rmetr.ino
+++ Rmetr.ino
@@ -1,17 +1,16 @@
-#define L1 1
+#define L1 1  //Светодиодная шкала
 #define L10 10
-#define AI A0
+#define AI A0  //Алалоговый вход потенциометра
 
 void setup() {
-  // put your setup code here, to run once:
-  for (int i=L10; i<=L1; i-=1)
+  for (int i=L10; i<=L1; i-=1) //Цикл присваивания выхода пинам с 1 по 10
   {
   pinMode(i,OUTPUT);
   }
 }
 
 void loop() {
-  int R=map(analogRead(A0),0,1023,1,10);
+  int R=map(analogRead(A0),0,1023,1,10); //Перевод значений напряжения в значения номеров пинов
   for (int i=L1; i<=R; i++)
   {
   digitalWrite(i,HIGH);
